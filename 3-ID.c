d591dc7ab66c1c8f17c79798152d0a955731a238
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-03-12 18:40:40 -0500

    adding testing files

---
+++ 3-ID.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/*
+*/int _numbers(va_list arg, int *k)
+{
+	unsigned int i, p, r;
+/*
+*/int n = va_arg(arg, int), num = 0;
+	if (n < 0)
+	{
+		_putchar(('-'), k);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+	p = 1;
+	r = i;
+
+	while (r > 9)
+	{
+		p *= 10;
+		r /= 10;
+	}
+
+	for (; p >= 1; p /= 10)
+	{
+		_putchar((((i / p) % 10) + '0'), k);
+		num++;
+	}
+	return (num);
+}
